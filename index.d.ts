bbe804cb1b36f700a41a245da17dce0528dc6ba8
Sean Cheung <theoxuanx@gmail.com>
2018-10-15 00:54:38 +0800

    added types definition

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+type Environments = Partial<Record<string, string>>;
+interface Utils {
+    /**
+     * Deep clone an object
+     *
+     * @param src Clone source
+     */
+    clone(src: any): any;
+
+    /**
+     * Make clones of two objects then merge the second one into the first one and returns the merged object
+     *
+     * @param src Merge source
+     * @param tar Merge target
+     */
+    merge(src: any, tar): any;
+
+    /**
+     * Read envs from file path
+     *
+     * @param file Env file path
+     * @param inject Inject envs into NODE_ENV
+     */
+    env(file: string, inject?: boolean): Environments;
+
+    /**
+     * Resolve source config
+     *
+     * @param src Source config
+     * @param envs Env vars
+     */
+    resolve(src: any, envs?: Environments): any;
+
+    /**
+     * Load config
+     *
+     * @param file Config file path
+     * @param inject Env vars
+     */
+    load(file: string, envs?: boolean): any;
+}
+
+declare interface Config {
+    readonly __: Utils;
+    readonly [x: string]: any;
+}
+
+declare const config: Config;
+export = config;
