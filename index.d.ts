cb429128baed81bf1277ca1b3a82087a5d75c99d
Zhang Xuan <xuan.zhang@xiaobao100.com>
2018-11-13 10:33:51 +0800

    types update

--- index.d.ts
+++ index.d.ts
@@ -1,54 +1,54 @@
-type Environments = Partial<Record<string, string>>;
-interface Utils {
-    /**
-     * Deep clone an object
-     *
-     * @param src Clone source
-     */
-    clone(src: any): any;
+declare const config: config.Config;
+declare namespace config {
+    type Environments = Partial<Record<string, string>>;
+    interface Config {
+        readonly __: Utils;
+        readonly [x: string]: any;
+    }
+    interface Utils {
+        /**
+         * Deep clone an object
+         *
+         * @param src Clone source
+         */
+        clone(src: any): any;
 
-    /**
-     * Make clones of two objects then merge the second one into the first one and returns the merged object
-     *
-     * @param src Merge source
-     * @param tar Merge target
-     */
-    merge(src: any, tar: any): any;
+        /**
+         * Make clones of two objects then merge the second one into the first one and returns the merged object
+         *
+         * @param src Merge source
+         * @param tar Merge target
+         */
+        merge(src: any, tar: any): any;
 
-    /**
-     * Read envs from file path
-     *
-     * @param file Env file path
-     * @param inject Inject envs into NODE_ENV
-     */
-    env(file: string, inject?: boolean): Environments;
+        /**
+         * Read envs from file path
+         *
+         * @param file Env file path
+         * @param inject Inject envs into NODE_ENV
+         */
+        env(file: string, inject?: boolean): Environments;
 
-    /**
-     * Resolve source config
-     *
-     * @param src Source config
-     * @param envs Env vars
-     */
-    resolve(src: any, envs?: Environments): any;
+        /**
+         * Resolve source config
+         *
+         * @param src Source config
+         * @param envs Env vars
+         */
+        resolve(src: any, envs?: Environments): any;
 
-    /**
-     * Load config
-     *
-     * @param file Config file path
-     * @param inject Env vars
-     */
-    load(file: string, envs?: boolean): any;
+        /**
+         * Load config
+         *
+         * @param file Config file path
+         * @param inject Env vars
+         */
+        load(file: string, envs?: boolean): any;
 
-    /**
-     * Clear cached config
-     */
-    desolve(): void
+        /**
+         * Clear cached config
+         */
+        desolve(): void;
+    }
 }
-
-declare interface Config {
-    readonly __: Utils;
-    readonly [x: string]: any;
-}
-
-declare const config: Config;
 export = config;
