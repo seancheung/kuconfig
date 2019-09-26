e8ccb481a718a1194f4baf4af7bb3f859df32ada
Zhang Xuan <xuan.zhang@xiaobao100.com>
2019-09-26 14:31:28 +0800

    added regex exp

--- tests/string.spec.js
+++ tests/string.spec.js
@@ -15,6 +15,13 @@ describe('string test', function() {
         expect(config.split3).to.eql(['a', 'b']);
         expect(config.expand1).to.eql('Tom is very good');
         expect(config.expand2).to.eql('Tom and ${expandvar2} are very good');
+        expect(config.regex1).to.be.an.instanceOf(RegExp);
+        expect(config.regex1.test('0.1.0')).to.be.true;
+        expect(config.regex1.test('0.1')).to.be.true;
+        expect(config.regex1.test('0.1.0.1')).to.be.false;
+        expect(config.regex2).to.be.an.instanceOf(RegExp);
+        expect(config.regex2.test('test')).to.be.true;
+        expect(config.regex2.test('TEST')).to.be.true;
     });
     after(function() {
         delete process.env.CONFIG_FILE;
