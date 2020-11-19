5f5480e2e4a0b0b96ba5ae65db5678b5fdbdc443
Zhang Xuan <xuan.zhang@xiaobao100.com>
2020-11-19 14:32:14 +0800

    metro plugin complete

--- plugins/metro.js
+++ plugins/metro.js
@@ -1,20 +1,24 @@
 let upstreamTransformer = null;
+const _require = file =>
+    require(require.resolve(file, { paths: [process.cwd()] }));
 
-const { version } = require('react-native/package.json');
+const { version } = _require('react-native/package.json');
 const [major, minor, patch] = version.split('.');
 if (minor >= 59) {
-    upstreamTransformer = require('metro-react-native-babel-transformer');
+    upstreamTransformer = _require('metro-react-native-babel-transformer');
 } else if (minor >= 56) {
-    upstreamTransformer = require('metro/src/reactNativeTransformer');
+    upstreamTransformer = _require('metro/src/reactNativeTransformer');
 } else if (minor >= 52) {
-    upstreamTransformer = require('metro/src/transformer');
+    upstreamTransformer = _require('metro/src/transformer');
 } else if (minor >= 47) {
-    upstreamTransformer = require('metro-bundler/src/transformer');
+    upstreamTransformer = _require('metro-bundler/src/transformer');
 } else if (minor == 46) {
-    upstreamTransformer = require('metro-bundler/build/transformer');
+    upstreamTransformer = _require('metro-bundler/build/transformer');
 } else {
     // handle RN <= 0.45
-    const oldUpstreamTransformer = require('react-native/packager/transformer');
+    const oldUpstreamTransformer = _require(
+        'react-native/packager/transformer'
+    );
     upstreamTransformer = {
         transform({ src, filename, options }) {
             return oldUpstreamTransformer.transform(src, filename, options);
@@ -23,10 +27,8 @@ if (minor >= 59) {
 }
 
 const { name } = require('../package.json');
-const modulePath = path.resolve(
-    require.resolve(name, { paths: [process.cwd()] })
-);
-const config = mode === 'override' ? require('../env') : require('../index');
+const modulePath = require('path').relative(process.cwd(), require.resolve(name, { paths: [process.cwd()] }))
+const config = process.env.KUCONFIG_MODE === 'override' ? require('../env') : require('../index');
 const moduleContent = `module.exports=${JSON.stringify(config)
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029')}`;
