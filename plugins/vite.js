739e003b68ab52e368cbd8fc4b2b43fd6d1fcdce
zhangxuan11 <theoxuanx@gmail.com>
2022-01-04 14:22:43 +0800

    fix: fix vite plugin issue

--- plugins/vite.js
+++ plugins/vite.js
@@ -1,10 +1,11 @@
 function kuconfigPlugin(opts) {
     const { name } = require('../package.json');
-
+    const regex = new RegExp(`${name}\\/index.js(\\?\\S+)?$`)
+  
     return {
         name: 'kuconfig',
-        load(id) {
-            if (id.endsWith(name + '/index.js')) {
+        load(source) {
+            if (regex.test(source)) {
                 const id =
                     opts?.mode === 'envs' ? '../override' : '../index';
                 return `export default ${JSON.stringify(require(id))
@@ -13,6 +14,7 @@ function kuconfigPlugin(opts) {
             }
         }
     };
-}
-module.exports = kuconfigPlugin;
-kuconfigPlugin['default'] = kuconfigPlugin;
+  }
+  module.exports = kuconfigPlugin;
+  kuconfigPlugin['default'] = kuconfigPlugin;
+  
